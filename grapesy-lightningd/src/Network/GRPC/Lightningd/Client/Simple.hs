6505dc2f6ff5126391a3b947ce81b0379f5959b1
Daniel Firth <dan.firth@homotopic.tech>
2025-11-07 16:20:41 +0000

    Fix -Werror check

--- grapesy-lightningd/src/Network/GRPC/Lightningd/Client/Simple.hs
+++ grapesy-lightningd/src/Network/GRPC/Lightningd/Client/Simple.hs
@@ -5,8 +5,7 @@ module Network.GRPC.Lightningd.Client.Simple (
 import Control.Monad.Catch (MonadMask)
 import Control.Monad.IO.Class (MonadIO)
 import Network.GRPC.Client (Connection, rpc)
-import Network.GRPC.Client.StreamType.IO (biDiStreaming, nonStreaming)
-import Network.GRPC.Common.NextElem qualified as NextElem
+import Network.GRPC.Client.StreamType.IO (nonStreaming)
 import Network.GRPC.Common.Protobuf (Proto, Protobuf)
 import Network.GRPC.Lightningd qualified as LN
 
